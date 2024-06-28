e34738d06617974a37d25d3caf78234dcd9b9b58
Pong <pongpong34756@gmail.com>
2024-06-28 16:26:23 +0700

    mongoDB HW express

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,68 @@
 import { Router } from "express";
+import { db } from "../utils/db";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  const collection = db.collection("products");
 
-productRouter.get("/:id", (req, res) => {});
+  return res.json({
+    data: "[<Product object หลายๆ อันจะอยู่ใน Array นี้>]",
+  });
+});
 
-productRouter.post("/", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = ObjectId(req.params.productId);
+  const newProductdata = { ...req.body };
 
-productRouter.put("/:id", (req, res) => {});
+  await collection.findOne({
+    _id: productId,
+  });
+  return res.json({
+    message: "recive products",
+  });
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("products");
+
+  const porductData = { ...req.body };
+  const products = await collection.insertOne(porductData);
+
+  return res.json({
+    message: "Product has been created successfully",
+  });
+});
+
+productRouter.put("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = ObjectId(req.params.productId);
+  const newProductdata = { ...req.body };
+
+  await collection.updateOne(
+    {
+      _id: productId,
+    },
+    {
+      $set: newProductdata,
+    }
+  );
+  return res.json({
+    message: "Product has been updated successfully",
+  });
+});
+
+productRouter.delete("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = ObjectId(req.params.productId);
+
+  await collection.deleteOne({
+    _id: productId,
+  });
+  return res.json({
+    message: "Product has been deleted successfully",
+  });
+});
 
 export default productRouter;
